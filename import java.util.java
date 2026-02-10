b7fc6c9f49d344c58c035aafaae74e87b8f3d4e6
KRRISH DEVANSH <kd1500@srmist.edu.in>
2026-02-10 13:50:38 +0530

    Add files via upload

---
+++ import java.util.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[])
+    Scanner so = newScanner(System.in);
+    {
+        string ch = so.next();
+        System.out.println(ch);
+    }
+}
